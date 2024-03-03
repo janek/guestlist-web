65dbd6cacc1c802af8331e473d91fa0823255498
Janek Szynal <jan.szynal@gmail.com>
2024-03-03 13:07:00 +0100

    Add global type for Database and use it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import GuestlistTable from "@/components/GuestlistTable";
 
 export default async function Page() {
   const cookieStore = cookies();
-  const supabase = createServerClient(
+  const supabase = createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
