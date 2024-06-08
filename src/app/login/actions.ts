0292937aac239917b095f5422ec2ff8cbc0be168
Janek Szynal <jan.szynal@gmail.com>
2024-06-08 16:19:18 +0200

    Clean up login actions

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -1,5 +1,4 @@
 "use server";
-
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
@@ -12,13 +11,10 @@ type LoginData = {
 
 export async function login(loginData: LoginData) {
   const supabase = createClient();
-
-  console.log("Login data:", loginData);
-
   const { error } = await supabase.auth.signInWithPassword(loginData);
 
   if (error) {
-    console.error("Login error:", error);
+    console.error("Login error:", error.message);
     return {
       message:
         error.message === "Invalid login credentials"
@@ -32,41 +28,39 @@ export async function login(loginData: LoginData) {
   redirect("/");
 }
 
-export async function signup(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  console.log("Signup data:", data);
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    console.error("Signup error:", error);
-    redirect("/error");
-  }
-
-  console.log("Signup successful, revalidating path and redirecting.");
-  revalidatePath("/", "layout");
-  redirect("/");
-}
-
 export async function logout() {
   const supabase = createClient();
-
   const { error } = await supabase.auth.signOut();
 
   if (error) {
-    console.error("Logout error:", error);
-    redirect("/error");
+    console.error("Logout error:", error.message);
+    return { message: "An error occurred during logout" };
   }
 
   console.log("Logout successful, revalidating path and redirecting.");
   revalidatePath("/", "layout");
   redirect("/login");
 }
+// export async function signup(formData: FormData) {
+//   const supabase = createClient();
+
+//   // type-casting here for convenience
+//   // in practice, you should validate your inputs
+//   const data = {
+//     email: formData.get("email") as string,
+//     password: formData.get("password") as string,
+//   };
+
+//   console.log("Signup data:", data);
+
+//   const { error } = await supabase.auth.signUp(data);
+
+//   if (error) {
+//     console.error("Signup error:", error);
+//     redirect("/error");
+//   }
+
+//   console.log("Signup successful, revalidating path and redirecting.");
+//   revalidatePath("/", "layout");
+//   redirect("/");
+// }
